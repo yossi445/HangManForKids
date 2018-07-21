a94088f874a116cf09f702ca5dafec137cd5716f
yossi445 <yossi445@gmail.com>
2018-07-21 17:33:41 +0300

    Initial commit + TheGameActivity layout design, stage 1

---
+++ app/src/main/java/yossi445/hangmanforkids/TheGame.java
@@ -0,0 +1,27 @@
+package yossi445.hangmanforkids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class TheGame extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_game);
+
+    }
+
+
+
+
+    public void onButtonClick(View view) {
+    }
+}
